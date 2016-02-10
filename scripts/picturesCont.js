2741ef20984b1122ab39eb21f663377aeea31bc4
TimHeaphy <theheaphy@gmail.com>
2016-02-10 16:22:20 -0500

    figuring slick

--- scripts/picturesCont.js
+++ scripts/picturesCont.js
@@ -1,7 +1,6 @@
 var app = angular.module('port');
 
 app.controller('picturesCont', function($scope){ 
-
 	//function that tiggles the triggerHead content (lol tiggles)
 	$scope.reveal = function(trigger) {
 		$(function() {
@@ -12,8 +11,7 @@ app.controller('picturesCont', function($scope){
 	$('.slickCarousel').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 2000,
+		autoplay: false
 	});
 });
 
